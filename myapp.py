cb656afa2999e6ebc00f8dbbb0c5738ecbc4f4ba
nicolasagudelo <nicoalbert95@hotmail.com>
2019-11-28 22:44:00 -0500

    Added comments to the files to explain the functioning of the application and the playbooks. Renamed the playbooks and redestributed the commands on different files to separate them by category

--- myapp.py
+++ myapp.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
-from flask import Flask,jsonify,render_template, request
+from flask import Flask,render_template, request
 from flaskext.mysql import MySQL
 
 
-
-
 app = Flask(__name__)
+
+'''
+Configuration required to access to MySQL database server
+'''
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
@@ -17,6 +19,12 @@ mysql = MySQL(app)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    '''
+    Depending on whether the application receives a POST or a GET request it will proceed differently if a POST request is received the application will attempt to store 
+    the information provided on index.html into the database and then proceed to show the user the success.html if the information was correctly stored into the database,
+    on the other hand if a GET request is received it will show index.html to the user.
+    '''
+
     if request.method == "POST":
         #print (app.config)
         details = request.form
@@ -31,6 +39,11 @@ def index():
         cursor.close()
         return render_template('success.html')
     return render_template('index.html')
+
+'''
+    If there is any error with the database and an Internal Server Error is created the following code will render error.html to let the user know about this and let him go
+    back to index.html
+'''
 
 @app.errorhandler(500)
 def internal_error(error):
